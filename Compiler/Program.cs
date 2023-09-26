0e7bf9a7df43766082c76e5337d1449d886a3d32
Andrey <konohov.andreyka@gmail.com>
2023-09-26 19:50:36 +0300

    add: token visualiser

--- Compiler/Program.cs
+++ Compiler/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+using Compiler;
 using Compiler.Imperative;
 
 var parser = new ImperativeParser();
@@ -9,6 +9,7 @@ var filename = args[0];
 // TODO: figure out how to work with output file in a better way
 File.Delete($"output.txt");
 parser.Parse(File.ReadAllText($"Assets/{filename}.txt"));
+TokenVisualiser.VisualiseTokensIntoSourceCode("output.txt", "output2.txt");
 // Console.WriteLine($"WE PARSED SOMETHING. WE GOT: {parser.SomeValue}");
 
 // TODO: take CLI arguments, compile code, run with args, output result
---
+++ Compiler/TokenVisualiser.cs
@@ -0,0 +1,38 @@
+using System.Text.RegularExpressions;
+
+namespace Compiler;
+
+public static class TokenVisualiser
+{
+    public static void VisualiseTokensIntoSourceCode(string from, string to)
+    {
+        File.Delete(to);
+        var tokens = File.ReadAllText(from).Split(new string[] { Environment.NewLine + Environment.NewLine },
+            StringSplitOptions.RemoveEmptyEntries);
+        var line = 1;
+        foreach (var tk in tokens)
+        {
+            var match = Regex.Match(tk,
+                @"Token of type ([A-Z_]+) encountered\.\r?\n(\d+):(\d+) - \d+:\d+\r?\nUnderlying string: (.+)");
+            var tokenType = match.Groups[1].Value;
+            var lineNumber = int.Parse(match.Groups[2].Value);
+            var column = int.Parse(match.Groups[3].Value);
+            var uStr = match.Groups[4].Value;
+            string textToWrite;
+            if (line == lineNumber)
+            {
+                textToWrite = tokenType;
+            }
+            else
+            {
+                textToWrite = new string('\n', lineNumber - line) + new string(' ', column) + tokenType;
+                line = lineNumber;
+            }
+
+            textToWrite += tokenType == "IDENTIFIER" || tokenType == "INT_LITERAL" || tokenType == "REAL_LITERAL"
+                ? $"({uStr}) "
+                : " ";
+            File.AppendAllText(to, textToWrite);
+        }
+    }
+}
